4fad256929d8fc2a139d8207aab4f4760b751041
faizii-x <faizanramzan670@gmail.com>
2024-03-18 13:09:07 +0500

    vetrans

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,41 +1,21 @@
-// import { Inter } from "next/font/google";
-// import "./globals.css";
-
-// // const inter = Inter({ subsets: ["latin"] });
-// const { Inter } = require("next/font/google");
-
-// export const metadata = {
-//   title: "Vetrans",
-//   description: "Construction Website",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body className={inter.className}>{children}</body>
-//     </html>
-//   );
-// }
-
 import { Inter } from "next/font/google";
-import { GoogleFonts } from "next-google-fonts";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
- export const metadata = {
-     title: "Vetrans",
-     description: "Construction Website",
-   };
-  
-function MyApp({ children }) {
+export const metadata = {
+  title: "Vetrans",
+  description: "Construction Website",
+};
+
+export default function RootLayout({ children }) {
   return (
-    <>
-      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" />
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   );
 }
 
-export default MyApp;
+
+
+
